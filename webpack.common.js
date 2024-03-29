8d88cb3e07b887afc781c5224523fc128fafd3d3
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-03-29 22:53:12 +0530

    Setup project

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        bundle: path.resolve(__dirname, "src/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name][contenthash].js",
+        clean: true,
+        assetModuleFilename: "[name][ext]",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.module\.css$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: { exportLocalsConvention: "camelCase" },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            favicon: "./src/assets/favicon.png",
+        }),
+    ],
+};
