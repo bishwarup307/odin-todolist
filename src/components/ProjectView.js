ee8e0768a6e8df6a1eea5250c3254a15699de2b3
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-04-17 22:43:03 +0530

    Add project search

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -10,6 +10,26 @@ export default function ProjectView() {
     const container = document.createElement("div");
     container.className = "task-container mt-16 xl:flex";
 
+    const searchDiv = document.createElement("div");
+    searchDiv.className = "relative";
+    const searchInput = document.createElement("input");
+    searchInput.className =
+        "mb-2 rounded-full bg-slate-100 w-full px-4 py-2 border-black border-[1px] shadow-slate-100 shadow-md text-md placeholder:text-sm focus:outline-none focus:bg-white focus:border-2";
+    searchInput.placeholder = "Search project";
+    searchDiv.appendChild(searchInput);
+
+    searchInput.addEventListener("input", () => {
+        // console.log("fired");
+        updateSidebar(searchInput.value);
+    });
+
+    const searchIcon = document.createElement("div");
+    searchIcon.innerHTML = `<svg viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.132 9.71395C10.139 11.2496 10.3328 13.2665 11.6 14.585C12.8468 15.885 14.8527 16.0883 16.335 15.065C16.6466 14.8505 16.9244 14.5906 17.159 14.294C17.3897 14.0023 17.5773 13.679 17.716 13.334C18.0006 12.6253 18.0742 11.8495 17.928 11.1C17.7841 10.3573 17.4268 9.67277 16.9 9.12995C16.3811 8.59347 15.7128 8.22552 14.982 8.07395C14.2541 7.92522 13.4982 8.00197 12.815 8.29395C12.1254 8.58951 11.5394 9.08388 11.132 9.71395Z" stroke="#cfcfcf" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M17.5986 13.6868C17.2639 13.4428 16.7947 13.5165 16.5508 13.8513C16.3069 14.1861 16.3806 14.6552 16.7154 14.8991L17.5986 13.6868ZM19.0584 16.6061C19.3931 16.85 19.8623 16.7764 20.1062 16.4416C20.3501 16.1068 20.2764 15.6377 19.9416 15.3938L19.0584 16.6061ZM7.5 12.7499C7.91421 12.7499 8.25 12.4142 8.25 11.9999C8.25 11.5857 7.91421 11.2499 7.5 11.2499V12.7499ZM5.5 11.2499C5.08579 11.2499 4.75 11.5857 4.75 11.9999C4.75 12.4142 5.08579 12.7499 5.5 12.7499V11.2499ZM7.5 15.7499C7.91421 15.7499 8.25 15.4142 8.25 14.9999C8.25 14.5857 7.91421 14.2499 7.5 14.2499V15.7499ZM5.5 14.2499C5.08579 14.2499 4.75 14.5857 4.75 14.9999C4.75 15.4142 5.08579 15.7499 5.5 15.7499V14.2499ZM8.5 9.74994C8.91421 9.74994 9.25 9.41415 9.25 8.99994C9.25 8.58573 8.91421 8.24994 8.5 8.24994V9.74994ZM5.5 8.24994C5.08579 8.24994 4.75 8.58573 4.75 8.99994C4.75 9.41415 5.08579 9.74994 5.5 9.74994V8.24994ZM16.7154 14.8991L19.0584 16.6061L19.9416 15.3938L17.5986 13.6868L16.7154 14.8991ZM7.5 11.2499H5.5V12.7499H7.5V11.2499ZM7.5 14.2499H5.5V15.7499H7.5V14.2499ZM8.5 8.24994H5.5V9.74994H8.5V8.24994Z" fill="#cfcfcf"></path> </g></svg>`;
+    searchIcon.className = "absolute w-10 top-0 right-2";
+    searchDiv.appendChild(searchIcon);
+
+    // container.appendChild(searchDiv);
+
     const btnAdd1 = AddProjectBtn();
     container.appendChild(btnAdd1);
     btnAdd1.classList.add("xl:hidden");
@@ -45,49 +65,70 @@ export default function ProjectView() {
     const sidebar = document.createElement("div");
     sidebar.className = "hidden flex-col xl:flex xl:min-w-64 xl:mr-16";
 
+    sidebar.appendChild(searchDiv);
+
     const btnAdd2 = AddProjectBtn();
-    // btnAdd2.className = "hidden xl:flex";
     btnAdd2.classList.add("hidden");
     btnAdd2.classList.add("xl:flex");
     btnAdd2.classList.add("xl:mb-12");
     sidebar.appendChild(btnAdd2);
 
-    projects.forEach((project) => {
-        const projectBtn = document.createElement("button");
-        projectBtn.className =
-            "project-nav-btn flex items-center justify-center px-4 py-2 rounded-lg bg-textPrimary border-b-[1px] border-bgExtraLight text-white transition-all duration-300 xl:rounded-none xl:bg-white xl:text-textPrimary xl:font-medium xl:justify-start";
+    function updateSidebar(searchTerm) {
+        console.log(sidebar.children.length);
 
-        if (selected) {
-            if (project.id === selected.id) {
-                selected = projectBtn;
+        [...sidebar.children].forEach((btn) => {
+            if (btn.classList.contains("project-nav-btn")) {
+                console.log(`removing ${btn.dataset.name}`);
+                sidebar.removeChild(btn);
             }
-        } else {
-            if (project.name === "default") {
-                selected = projectBtn;
+        });
+
+        let renderProjects = [];
+        if (searchTerm) {
+            renderProjects = projects.filter((item) => {
+                const searchFor = util.processString(searchTerm);
+                return util.processString(item.name).match(searchFor);
+            });
+        } else renderProjects = projects;
+        console.log(renderProjects.length);
+
+        renderProjects.forEach((project) => {
+            const projectBtn = document.createElement("button");
+            projectBtn.className =
+                "project-nav-btn flex items-center justify-center px-4 py-2 rounded-lg bg-textPrimary border-b-[1px] border-bgExtraLight text-white transition-all duration-300 xl:rounded-none xl:bg-white xl:text-textPrimary xl:font-medium xl:justify-start";
+
+            if (selected) {
+                if (project.id === selected.id) {
+                    selected = projectBtn;
+                }
+            } else {
+                if (project.name === "default") {
+                    selected = projectBtn;
+                }
             }
-        }
-        selected.classList.add("active");
-
-        projectBtn.textContent = project.name;
-        projectBtn.id = project.id;
-        projectBtn.dataset.name = project.name;
-        sidebar.appendChild(projectBtn);
-
-        projectBtn.addEventListener("click", () => {
-            selected.classList.remove("active");
-            selected = projectBtn;
             selected.classList.add("active");
 
-            for (let child of container.children) {
-                if (child.id === "projectview-task-container")
-                    container.removeChild(child);
-            }
-            container.appendChild(displayTasks(projectBtn.id));
-            currentProject.textContent = project.name;
-            sidebar.classList.add("hidden");
-        });
-    });
+            projectBtn.textContent = project.name;
+            projectBtn.id = project.id;
+            projectBtn.dataset.name = project.name;
+            sidebar.appendChild(projectBtn);
 
+            projectBtn.addEventListener("click", () => {
+                selected.classList.remove("active");
+                selected = projectBtn;
+                selected.classList.add("active");
+
+                for (let child of container.children) {
+                    if (child.id === "project-view-task-container")
+                        container.removeChild(child);
+                }
+                container.appendChild(displayTasks(projectBtn.id));
+                currentProject.textContent = project.name;
+                sidebar.classList.add("hidden");
+            });
+        });
+    }
+    updateSidebar();
     container.appendChild(sidebar);
     container.appendChild(displayTasks(selected.id));
 
@@ -109,7 +150,7 @@ function displayTasks(projectId) {
     const taskContainer = document.createElement("div");
     taskContainer.className =
         "flex-1 grid grid-cols-1 gap-y-8 items-start xl:grid-cols-2 xl:gap-x-12 dxl:grid-cols-3";
-    taskContainer.id = "projectview-task-container";
+    taskContainer.id = "project-view-task-container";
     const myTasks = TaskList.getByProject(projectId);
 
     myTasks.forEach((task) => {
@@ -143,7 +184,7 @@ function AddProjectModal() {
 
     const container = document.createElement("div");
     container.className =
-        "grid grid-cols-[100px_auto] gap-x-4 gap-y-8 items-center";
+        "grid grid-cols-[100px_auto] gap-x-4 gap-y-4 items-start";
 
     const labelName = document.createElement("p");
     labelName.textContent = "Name";
